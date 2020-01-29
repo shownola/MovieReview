1880f9bc8ea24b06ea1c5635f32f878b376ab9f5
shownola <swasieko@gmail.com>
2020-01-29 14:18:16 -0700

    Initial setup and testing of api request

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey:  '67fad89f',
+      i: 'tt0848228'
+    }
+  });
+  console.log(response.data);
+}
+
+fetchData();
