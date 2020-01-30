992b369c395069bd5d3f47f938607a321c2fc1bd
shownola <swasieko@gmail.com>
2020-01-29 18:18:06 -0700

    Rendered movies and handles errored responses

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const fetchData = async (searchTerm) => {
       s: searchTerm
     }
   });
-  console.log(response.data);
+
+  if (response.data.Error){
+    return [];
+  };
+  return response.data.Search;
 };
 
 const input = document.querySelector('input');
 
-const onInput = event => {
-  fetchData(event.target.value);
+const onInput = async event => {
+  const movies = await fetchData(event.target.value);
+  for (let movie of movies){
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+      <img src="${movie.Poster}" />
+      <h1>${movie.Title}</h1>
+    `;
+    document.querySelector('#target').appendChild(div);
+  }
 };
 input.addEventListener('input', debouce(onInput, 500));
