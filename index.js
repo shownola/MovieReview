1dc1e7787658d54b8712c5d619a48db851ccf966
shownola <swasieko@gmail.com>
2020-01-30 16:00:37 -0700

    Added a new search for the ability to have a side by side comparison of movies

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-
-createAutoComplete({
-  root: document.querySelector('.autocomplete'),
+const autoCompleteConfig = {
   renderOption(movie){
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
     return `
       <img src="${imgSrc}" />
       ${movie.Title} (${movie.Year})
     `;
-  },
-  onOptionSelect(movie){
-    onMovieSelect(movie);
   },
   inputValue(movie){
     return movie.Title;
@@ -27,10 +22,26 @@ createAutoComplete({
     };
     return response.data.Search;
   }
+};
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#left-autocomplete'),
+  onOptionSelect(movie){
+    document.querySelector('.tutorial').classList.add('is-hidden');
+    onMovieSelect(movie, document.querySelector('#left-summary'));
+  },
+});
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#right-autocomplete'),
+  onOptionSelect(movie){
+    document.querySelector('.tutorial').classList.add('is-hidden');
+    onMovieSelect(movie, document.querySelector('#right-summary'));
+  },
 });
 
 
-const onMovieSelect = async movie => {
+const onMovieSelect = async (movie, summaryElement) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey:  '67fad89f',
@@ -38,7 +49,7 @@ const onMovieSelect = async movie => {
     }
   });
   console.log(response.data);
-  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+  summaryElement.innerHTML = movieTemplate(response.data);
 };
 
 const movieTemplate = (movieDetail) => {
