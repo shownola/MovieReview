424a03af52c90fb36ec418e23f6b1c904ad41ad0
shownola <swasieko@gmail.com>
2020-01-30 11:11:01 -0700

    Rendered the show page for selected movies

--- index.js
+++ index.js
@@ -71,4 +71,47 @@ const onMovieSelect = async movie => {
     }
   });
   console.log(response.data);
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
 };
+
+const movieTemplate = (movieDetail) => {
+  return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" alt="poster" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre} - ${movieDetail.Year}</h4>
+          <h5>${movieDetail.Actors}</h5>
+          <p>${movieDetail.imdbRating} /  ${movieDetail.Awards}</p>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+    <article class="notification is-primary">
+      <p class="">${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="">${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="">${movieDetail.imbdRating}</p>
+      <p class="subtitle">Rating</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="">${movieDetail.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="">${movieDetail.imdbVotes}</p>
+      <p class="subtitle">Votes</p>
+    </article>
+
+  `
+}
