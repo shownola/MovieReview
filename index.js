061e66b46d0e5fb19bb794876a93a08246407a75
shownola <swasieko@gmail.com>
2020-01-30 13:39:52 -0700

    Complete refactor of autocomplete function

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-const fetchData = async (searchTerm) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey:  '67fad89f',
-      s: searchTerm
-    }
-  });
-
-  if (response.data.Error){
-    return [];
-  };
-  return response.data.Search;
-};
 
 createAutoComplete({
-  root: document.querySelector('.autocomplete')
+  root: document.querySelector('.autocomplete'),
+  renderOption(movie){
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+    return `
+      <img src="${imgSrc}" />
+      ${movie.Title} (${movie.Year})
+    `;
+  },
+  onOptionSelect(movie){
+    onMovieSelect(movie);
+  },
+  inputValue(movie){
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey:  '67fad89f',
+        s: searchTerm
+      }
+    });
+
+    if (response.data.Error){
+      return [];
+    };
+    return response.data.Search;
+  }
 });
-createAutoComplete({
-  root: document.querySelector('.autocomplete-two')
-});
-createAutoComplete({
-  root: document.querySelector('.autocomplete-three')
-});
+
 
 const onMovieSelect = async movie => {
   const response = await axios.get('http://www.omdbapi.com/', {
